465f1f0cf76b0671a0f2161570315dd9b937f385
gilsonalves <gilsonalves.developer@gmail.com>
2020-02-07 02:16:27 -0300

    Adicionando lógica para criar requisição POST

--- src/main/groovy/HttpAcces.groovy
+++ src/main/groovy/HttpAcces.groovy
@@ -1,26 +1,39 @@
+import com.google.gson.Gson
+import org.apache.http.HttpEntity
 import org.apache.http.HttpHeaders
+import org.apache.http.NameValuePair
 import org.apache.http.client.config.RequestConfig
+import org.apache.http.client.entity.UrlEncodedFormEntity
 import org.apache.http.client.methods.HttpUriRequest
 import org.apache.http.client.methods.RequestBuilder
+import org.apache.http.entity.StringEntity
 import org.apache.http.impl.client.HttpClientBuilder
 import org.apache.http.util.EntityUtils
 
 class HttpAcces {
-
 
     List<String> sendRequest(String url, String method, Map<String,Object> body = null) {
         RequestConfig requestConfig = RequestConfig.custom()
                 .setConnectTimeout(2000)
                 .setSocketTimeout(3000)
                 .build()
+        HttpUriRequest request = null
 
-       // StringEntity entity = new StringEntity(new Gson().toJson(body), "UTF-8")
-
-        HttpUriRequest request = RequestBuilder.create(method)
-                .setConfig(requestConfig)
-                .setUri(url)
-                .setHeader(HttpHeaders.CONTENT_TYPE, "text/html")
-                .build()
+        if (body){
+            StringEntity entity = new StringEntity(new Gson().toJson(body), "UTF-8")
+             request = RequestBuilder.create(method)
+                    .setConfig(requestConfig)
+                    .setUri(url)
+                    .setHeader(HttpHeaders.CONTENT_TYPE, "text/html")
+                    .setEntity(entity)
+                    .build()
+        } else {
+            request = RequestBuilder.create(method)
+                    .setConfig(requestConfig)
+                    .setUri(url)
+                    .setHeader(HttpHeaders.CONTENT_TYPE, "text/html")
+                    .build()
+        }
 
         String req = "REQUEST:" + "\n" + request.getRequestLine() + "\n" + "Headers: " +
                 request.getAllHeaders() + "\n" + EntityUtils.toString() + "\n"
--- src/test/groovy/HttpAccesSpec.groovy
+++ src/test/groovy/HttpAccesSpec.groovy
@@ -5,12 +5,20 @@ class HttpAccesSpec extends Specification {
     void 'testa resultado requisicao'() {
         setup:
         HttpAcces httpAcces = new HttpAcces()
-        String url = 'http://localhost:8080/hello'
+        String url = 'http://localhost:8080/login'
 
         when:
-        List resultado = httpAcces.sendRequest(url, 'GET')
+        List login = httpAcces.sendRequest(url, 'GET')
+        String token = login.get(1).find('(?<=name="_csrf" type="hidden" value=")[\\w-]+')
+        Map<String, Object> params = [
+                'username': 'admin',
+                'password': 'admin',
+                '_csrf'   : token
+        ]
+        List paginaInicial = httpAcces.sendRequest(url, 'POST', params)
 
         then:
-        resultado
+        paginaInicial
+        login
     }
 }
